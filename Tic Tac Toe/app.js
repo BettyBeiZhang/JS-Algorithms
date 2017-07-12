b2f647c15da617021ce75931ff70a1bf64f195b8
bei zhang <beizhang18@yahoo.com>
2017-07-11 23:19:39 -0700

    added isGameFinished functionality

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -1,30 +1,85 @@
-function startGame() {
-  document.turn = "X";
-  //setMessage(document.turn + " gets to start");
-}
+// Immediately-invoked function expression
+(function () {
+  var turn = "X";
+  var table = document.getElementById('game-table');
+  var board = ['E','E','E','E','E','E','E','E','E','E'];
+  var result = "still running";
 
-function setMessage(msg) {
-  var str = msg + " gets to start";
-  document.getElementById("message").innerText = str;
-}
+  //console.log(table);
+  table.addEventListener('click', function(event) {
+    //console.log(event);
+    if (event.target.tagName != "TD") {
+      return;
+    };
+    if (!event.target._clicked) {
+      event.target.innerText = turn;
+      setMessage(turn + "'s turn", "message");
 
-
-var table = document.getElementById('game-table');
-
-//console.log(table);
-table.addEventListener('click', function(event) {
-  //console.log(event);
-  if (event.target.tagName != "TD") {
-    return;
-  };
-  if (!event.target._clicked) {
-    event.target.innerText = document.turn;
-    setMessage(document.turn);
-    if (document.turn == "X") {
-      document.turn = "O";
-    } else {
-      document.turn = "X";
+      if (turn == "X") {
+        turn = "O";
+      } else {
+        turn = "X";
+      }
+      event.target._clicked = true;
     }
-    event.target._clicked = true;
+
+
+    board[event.target.id] = getBox(event.target.id);
+    isGameDone();
+    setMessage(result, "result");
+  }, false);
+
+  function setMessage(msg,idString) {
+    document.getElementById(idString).innerText = msg;
+
   }
-}, false);
+
+  function getBox(number) {
+   return document.getElementById(number).innerText;
+  }
+
+  function isGameDone(){
+    for(var i = 0; i <= 6; i = i + 3) {
+            if(board[i] !== "E" && board[i] === board[i + 1] && board[i + 1] == board[i + 2]) {
+                result = board[i] + " won"; //update the state result
+                return true;
+            }
+        }
+
+        //check columns
+        for(var i = 0; i <= 2 ; i++) {
+            if(board[i] !== "E" && board[i] === board[i + 3] && board[i + 3] === board[i + 6]) {
+                result = board[i] + " won"; //update the state result
+                return true;
+            }
+        }
+
+        //check diagonals
+        for(var i = 0, j = 4; i <= 2 ; i = i + 2, j = j - 2) {
+            if(board[i] !== "E" && board[i] == board[i + j] && board[i + j] === board[i + 2*j]) {
+                result = board[i] + " won"; //update the state result
+                return true;
+            }
+        }
+        var available = emptyCells();
+        if(available.length == 0) {
+            //the game is draw
+            result = "draw"; //update the state result
+            return true;
+        }
+        else {
+            return false;
+        }
+  }
+
+  function emptyCells() {
+    var indxs = [];
+       for(var itr = 0; itr < 9 ; itr++) {
+           if(board[itr] === "E") {
+               indxs.push(itr);
+           }
+       }
+       return indxs;
+  }
+
+})();
